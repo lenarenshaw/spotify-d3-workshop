d2c5e602fa8fd9be74bdf51f9b1cc80b6e9a3bc6
Kevin Du <kevin_du@brown.edu>
2020-01-22 19:47:20 -0500

    added stencil

--- solution/bubblechart.js
+++ solution/bubblechart.js
@@ -3,31 +3,6 @@ const CLIENT_SECRET = '7424d153e69746baa5532853207b1d40'
 const ACCESS_TOKEN = 'BQCqXkC5YvH8rxTkzEWvKRZrv7RjKcat1IZ5ErsrDRRqftU5XqK4COE2jvrQ54_oXeDhZriPWAUeL-Nx1noHyhZ-7XTOAC0Htr8C_b7KdJqSkFw9BRFQOXYmCR6u_dgDBON3x-lQDE2PwT8cyNHJzfCe'
 
 async function getAndVisualizeSpotifyData(){
-    // Lena todo!
-    // dataset = {
-    //     "children": [{"Name":"Olives","Count":4319},
-    //         {"Name":"Tea","Count":4159},
-    //         {"Name":"Mashed Potatoes","Count":2583},
-    //         {"Name":"Boiled Potatoes","Count":2074},
-    //         {"Name":"Milk","Count":1894},
-    //         {"Name":"Chicken Salad","Count":1809},
-    //         {"Name":"Vanilla Ice Cream","Count":1713},
-    //         {"Name":"Cocoa","Count":1636},
-    //         {"Name":"Lettuce Salad","Count":1566},
-    //         {"Name":"Lobster Salad","Count":1511},
-    //         {"Name":"Chocolate","Count":1489},
-    //         {"Name":"Apple Pie","Count":1487},
-    //         {"Name":"Orange Juice","Count":1423},
-    //         {"Name":"American Cheese","Count":1372},
-    //         {"Name":"Green Peas","Count":1341},
-    //         {"Name":"Assorted Cakes","Count":1331},
-    //         {"Name":"French Fried Potatoes","Count":1328},
-    //         {"Name":"Potato Salad","Count":1306},
-    //         {"Name":"Baked Potatoes","Count":1293},
-    //         {"Name":"Roquefort","Count":1273},
-    //         {"Name":"Stewed Prunes","Count":1268}]
-    // };
-
     $.ajax({
         url: 'https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=medium_term',
         headers: {
@@ -42,12 +17,6 @@ async function getAndVisualizeSpotifyData(){
             // TODO: Replace with whatever we're gonna do for this
             const color = d3.scaleOrdinal(d3.schemeCategory20);
 
-            // d3.pack is used to create nested circles. When there's no nesting and only children on same level, creates a bubble chart.
-            // see https://d3-wiki.readthedocs.io/zh_CN/master/Pack-Layout/
-            const bubble = d3.pack(dataset)
-                .size([diameter, diameter])
-                .padding(1.5);
-
             // Creates an empty svg object in the body of the html with specified width, height, and class.
             const svg = d3.select("body")
                 .append("svg")
@@ -60,6 +29,12 @@ async function getAndVisualizeSpotifyData(){
                 .sum(function(d) { console.log(d); return d.popularity; });
             console.log(nodes);
             console.log(bubble(nodes).leaves());
+
+            // d3.pack is used to create nested circles. When there's no nesting and only children on same level, creates a bubble chart.
+            // see https://d3-wiki.readthedocs.io/zh_CN/master/Pack-Layout/
+            const bubble = d3.pack(dataset)
+                .size([diameter, diameter])
+                .padding(1.5);
 
             // add a node class (object of type g) for each data
             const node = svg.selectAll(".node") // we can actually use any identifier that's not actually used here?
--- stencil/README.txt
+++ stencil/README.txt
@@ -1,1 +1,6 @@
-This is README for stencil code!
+This is readme for solution code!
+
+https://developer.spotify.com/documentation/web-api/quick-start/
+https://developer.spotify.com/console/get-current-user-top-artists-and-tracks/?type=artists&time_range=&limit=&offset=
+https://developer.spotify.com/documentation/general/guides/authorization-guide/#list-of-scopes
+https://developer.spotify.com/dashboard
